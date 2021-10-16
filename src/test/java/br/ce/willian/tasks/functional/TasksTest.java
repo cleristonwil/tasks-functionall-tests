2e84bb758bdccab0c2def48a117be6c592b627cd
cleriston <cleristonwil@gmail.com>
2021-10-16 18:15:15 -0300

    initial version

---
+++ src/test/java/br/ce/willian/tasks/functional/TasksTest.java
@@ -0,0 +1,117 @@
+package br.ce.willian.tasks.functional;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TasksTest {
+	
+	public WebDriver acessarAplicacao() {
+		WebDriver driver = new ChromeDriver();
+		driver.navigate().to("http://localhost:8001/tasks");
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		return driver;
+	}
+
+	@Test
+	public void deveSalvarTarefaComSucesso() {
+		WebDriver driver = acessarAplicacao();
+		try {
+		
+			//Clicar em Add Todo
+			driver.findElement(By.id("addTodo")).click();
+			
+			//Escrever a descrição
+			driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
+			
+			//Escrever a data
+			driver.findElement(By.id("dueDate")).sendKeys("16/10/2030");
+			
+			//Clicar em Salvar
+			driver.findElement(By.id("saveButton")).click();
+			
+			//Validar mensagem de sucesso
+			String message = driver.findElement(By.id("message")).getText();
+			Assert.assertEquals("Success!", message);
+		} finally {
+			//Fechar o Browser
+			driver.quit();
+		}
+	}
+		@Test
+		public void naoDeveSalvarTarefaSemDescricao() {
+			WebDriver driver = acessarAplicacao();
+			try {
+			
+				//Clicar em Add Todo
+				driver.findElement(By.id("addTodo")).click();
+											
+				//Escrever a data
+				driver.findElement(By.id("dueDate")).sendKeys("16/10/2030");
+				
+				//Clicar em Salvar
+				driver.findElement(By.id("saveButton")).click();
+				
+				//Validar mensagem de sucesso
+				String message = driver.findElement(By.id("message")).getText();
+				Assert.assertEquals("Fill the task description", message);
+			} finally {
+				//Fechar o Browser
+				driver.quit();
+			}
+		}
+		
+		@Test
+		public void naoDeveSalvarTarefaSemData() {
+			WebDriver driver = acessarAplicacao();
+			try {
+			
+				//Clicar em Add Todo
+				driver.findElement(By.id("addTodo")).click();
+				
+				//Escrever a descrição
+				driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
+				
+				//Clicar em Salvar
+				driver.findElement(By.id("saveButton")).click();
+				
+				//Validar mensagem de sucesso
+				String message = driver.findElement(By.id("message")).getText();
+				Assert.assertEquals("Fill the due date", message);
+			} finally {
+				//Fechar o Browser
+				driver.quit();
+			}
+		}
+		
+		@Test
+		public void naoDeveSalvarTarefaComDataPassada() {
+			WebDriver driver = acessarAplicacao();
+			try {
+			
+				//Clicar em Add Todo
+				driver.findElement(By.id("addTodo")).click();
+				
+				//Escrever a descrição
+				driver.findElement(By.id("task")).sendKeys("Teste via Selenium");
+				
+				//Escrever a data
+				driver.findElement(By.id("dueDate")).sendKeys("16/10/2010");
+				
+				//Clicar em Salvar
+				driver.findElement(By.id("saveButton")).click();
+				
+				//Validar mensagem de sucesso
+				String message = driver.findElement(By.id("message")).getText();
+				Assert.assertEquals("Due date must not be in past", message);
+			} finally {
+				//Fechar o Browser
+				driver.quit();
+			}
+		}
+}
+
